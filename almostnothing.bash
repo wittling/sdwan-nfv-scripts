1c0f7e8aa9fb9a18cf141937f26ceb8a0d02f408
wttling <mwittling@dispersivegroup.com>
2017-09-01 12:09:12 -0400

    change script to log to syslog

--- almostnothing.bash
+++ almostnothing.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Script that does almost nothing"
+SCRIPTNAME=almostnothing.bash
+MSG="Script that does almost nothing"
+
+# send string to std out and also stderr
+echo "Script: $SCRIPTNAME: $MSG" 2>&1
+# send same string to logger which puts it in syslog
+logger "Script: $SCRIPTNAME: $MSG"
+
+# Now we should be able to check and make sure these scripts are firing more effectively.
 exit 0
