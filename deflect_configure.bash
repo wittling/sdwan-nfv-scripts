65d7c15c301efa8f7e8832f70b1f731a4d886d3a
wittling <mwittling@dispersivegroup.com>
2017-09-18 19:35:38 -0400

    use new name bootstrapdsx

---
+++ deflect_configure.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+logger "CONFIGURATION of the Deflect"
+
+logger "Hostname: ${hostname}"
+logger "Traffic IP Address: ${deflect_ip}" 
+logger "Traffic Interface: ${trafficiface}" 
+logger "DSX IP Address: ${bootstrapdsx_ip}"
