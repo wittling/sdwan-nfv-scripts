f78d3247bf5ebdad697520c143fa90d9939f0e58
wittling <mwittling@dispersivegroup.com>
2017-09-18 19:30:02 -0400

    rename scripts

--- bootstrap-dsx-configure.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-logger "CONFIGURATION of the Bootstrap DSX"
-logger "Hostname: ${hostname}"
-logger "Control Plane IP: ${dsx-net}"
-logger "Control Plane Interface: ${ctlplaneiface}"
-logger "Reserved: ${reserved}"
