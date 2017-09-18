21840817eed8348f97afdf29eda6e97e8e3685ca
wittling <mwittling@dispersivegroup.com>
2017-09-18 19:32:05 -0400

    rename scripts per openbaton convention of source VNF then underscore then script name

--- deflect-configure.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-logger "CONFIGURATION of the Deflect"
-
-logger "Hostname: ${hostname}"
-logger "Traffic IP Address: ${dfl-net}" 
-logger "Traffic Interface: ${trafficiface}" 
-logger "DSX IP Address: ${dsxprimary_dsx-net}"
