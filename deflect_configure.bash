6186a91ca96b9d9b11f6a3829efe978f52495070
wittling <mwittling@dispersivegroup.com>
2017-09-19 16:25:51 -0400

    try to get configure script working

--- deflect_configure.bash
+++ deflect_configure.bash
@@ -2,7 +2,7 @@
 
 logger "deflect_configure: CONFIGURATION of the Deflect"
 
-logger "deflect_configure: Hostname: ${hostname}"
-logger "deflect_configure: Traffic IP Address: ${dflnet}" 
-logger "deflect_configure: Traffic Interface: ${ifacetraffic}" 
-logger "deflect_configure: DSX IP Address: ${dsxnet}"
+logger "deflect_configure: Hostname: ${deflect_hostname}"
+logger "deflect_configure: Traffic IP Address: ${deflect_dflnet}" 
+logger "deflect_configure: Traffic Interface: ${deflect_ifacetraffic}" 
+#logger "deflect_configure: DSX IP Address: bootstrapdsx_dsxnet"
