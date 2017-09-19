62fd90a6d166dd2f3828765fa8d7b557fe9137eb
wittling <mwittling@dispersivegroup.com>
2017-09-19 18:13:25 -0400

    adjustments per testing

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+env
+set -x
 logger "bootstrapdsx_configure.bash: CONFIGURATION of the Bootstrap DSX"
-logger "bootstrapdsx_configure.bash: Hostname: ${hostname}"
-logger "bootstrapdsx_configure.bash: Control Plane IP: ${dsxnet}"
-logger "bootstrapdsx_configure.bash: Control Plane Interface: ${ifacectlplane}"
-logger "bootstrapdsx_configure.bash: Reserved: ${reserved}"
+logger "bootstrapdsx_configure.bash: Hostname: $bootstrapdsx_hostname"
+logger "bootstrapdsx_configure.bash: Control Plane IP: $bootstrapdsx_dsxnet"
+logger "bootstrapdsx_configure.bash: Control Plane Interface: $bootstrapdsx_ifacectlplane"
+logger "bootstrapdsx_configure.bash: Reserved: $bootstrapdsx_reserved"
+set +x
