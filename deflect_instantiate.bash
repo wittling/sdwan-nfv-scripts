2dfc80c54e5e5142934b8a8d0d47512ec0cc6fc4
wittling <mwittling@dispersivegroup.com>
2017-09-19 10:07:46 -0400

    adding renamed files

---
+++ deflect_instantiate.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+logger "INSTANTIATION of the Deflect"
+
+logger "Hostname: ${hostname}"
+logger "IP Address: ${deflect_ip}" 
+logger "Traffic Interface: ${trafficiface}" 
+logger "Reserved: ${reserved}" 
