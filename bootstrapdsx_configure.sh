556a3017e67d33fdeb12d496b0c8a7da5d7c1758
wittling <mwittling@dispersivegroup.com>
2017-09-21 19:02:56 -0400

    more parm pass testing

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -6,6 +6,6 @@ logger "bootstrapdsx_configure.bash: CONFIGURATION"
 logger "bootstrapdsx_configure.bash: Hostname: ${hostname}"
 logger "bootstrapdsx_configure.bash: DSX IP: ${bootstrapdsx_dsxnet}"
 logger "bootstrapdsx_configure.bash: Traffic Interface: ${ifacetraffic}"
-logger "bootstrapdsx_configure.bash: DSX Control Plane Interface: ${ifacectlplane}"
+logger "bootstrapdsx_configure.bash: DSX Control Plane Interface: ${bootstrapdsx_ifacectlplane}"
 logger "bootstrapdsx_configure.bash: Registration Port: ${bootstrapdsx_regport}"
 #set +x
