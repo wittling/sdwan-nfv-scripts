f806d65a6789d9d73335b6c9ac5f2928561ca19f
wittling <mwittling@dispersivegroup.com>
2017-09-21 18:45:00 -0400

    add parameter

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-env
-set -x
+#env
+#set -x
 logger "bootstrapdsx_configure.bash: CONFIGURATION"
 logger "bootstrapdsx_configure.bash: Hostname: ${hostname}"
 logger "bootstrapdsx_configure.bash: DSX IP: ${bootstrapdsx_dsxnet}"
 logger "bootstrapdsx_configure.bash: Traffic Interface: ${ifacetraffic}"
 logger "bootstrapdsx_configure.bash: DSX Control Plane Interface: ${ifacectlplane}"
-logger "bootstrapdsx_configure.bash: Reserved: ${reserved}"
-set +x
+logger "bootstrapdsx_configure.bash: Registration Port: ${bootstrapdsx_regport}"
+#set +x
