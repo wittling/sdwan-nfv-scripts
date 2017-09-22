0d6cde62e10a4a95eb2c21f3b67b73fdd6969fd8
wittling <mwittling@dispersivegroup.com>
2017-09-22 13:35:13 -0400

    print statement refinement

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -2,12 +2,15 @@
 
 #env
 #set -x
-logger "bootstrapdsx_configure.bash: Greetings Deflect! I AM Bootstrap DSX...here to configure you!"
+logger "bootstrapdsx_configure.bash: Greetings Deflect! I am Bootstrap DSX."
 logger "bootstrapdsx_configure.bash: I see your Deflect Hostname has been assigned as: ${hostname}"
+logger "bootstrapdsx_configure.bash: My Bootstrap Hostname is: ${bootstrapdsx_hostname}."
 logger "bootstrapdsx_configure.bash: My Bootstrap DSX IP Address is: ${bootstrapdsx_dsxnet}."
 logger "bootstrapdsx_configure.bash: It appears you will using the traffic interface: ${ifacetraffic}"
 logger "bootstrapdsx_configure.bash: My Bootstrap DSX Control Plane Interface is: ${bootstrapdsx_ifacectlplane}"
 logger "bootstrapdsx_configure.bash: My Bootstrap DSX Registration Port is: ${bootstrapdsx_portreg}"
 logger "bootstrapdsx_configure.bash: My REST API Port is: ${bootstrapdsx_portrest}"
+logger "bootstrapdsx_configure.bash: When you contact me I will provision you and add you to a pool."
+
 logger "bootstrapdsx_configure.bash: Goodbye! Tchuss!"
 #set +x
--- deflect_configure.sh
+++ deflect_configure.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-logger "deflect_configure: Bootstrap DSX CONFIGURATION - Some parms passed in from Deflect!"
-
-logger "deflect_configure: Hostname: ${hostname}"
-logger "deflect_configure: IP Address: ${dflnet}" 
-logger "deflect_configure: Ctl Plane Interface: ${ifacectlplane}" 
-logger "deflect_configure: DSX IP Address: ${bootstrapdsx_dsxnet}"
-logger "deflect_configure: DSX Reg Port (passed param) ${bootstrapdsx_regport}"
-logger "deflect_configure: DSX REST API Port (passed param): ${bootstrapdsx_restport}"
+logger "deflect_configure: Greetings Bootstrap DSX! I am a Deflect."
+logger "deflect_configure: I see your hostname is: ${hostname}"
+logger "deflect_configure: My hostname is: ${deflect_hostname}"
+logger "deflect_configure: My Deflect IP Address is: ${deflect_dflnet}" 
+logger "deflect_configure: I see your IP Address is: ${dsxnet}"
+logger "deflect_configure: It appears you will be using the ctl plane interface: ${ifacectlplane}" 
+logger "deflect_configure: I will be sending data on port: ${deflect_portdata}" 
+logger "deflect_configure: I will be sending callp on port: ${deflect_portcallp}" 
+logger "Goodbye! Tchuss!"
