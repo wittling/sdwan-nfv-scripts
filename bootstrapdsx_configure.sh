f9a0167f14e754f014c19cb30b4add9ee7038f3c
wittling <mwittling@dispersivegroup.com>
2017-09-22 11:53:09 -0400

    parm passing additions

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -2,10 +2,11 @@
 
 #env
 #set -x
-logger "bootstrapdsx_configure.bash: CONFIGURATION"
+logger "bootstrapdsx_configure.bash: DEFLECT CONFIGURATION - SOME PARMS PASSED from DSX!"
 logger "bootstrapdsx_configure.bash: Hostname: ${hostname}"
-logger "bootstrapdsx_configure.bash: DSX IP: ${bootstrapdsx_dsxnet}"
+logger "bootstrapdsx_configure.bash: DSX IP (passed param): ${bootstrapdsx_dsxnet}"
 logger "bootstrapdsx_configure.bash: Traffic Interface: ${ifacetraffic}"
-logger "bootstrapdsx_configure.bash: DSX Control Plane Interface: ${bootstrapdsx_ifacectlplane}"
-logger "bootstrapdsx_configure.bash: Registration Port: ${bootstrapdsx_regport}"
+logger "bootstrapdsx_configure.bash: DSX Control Plane Interface (passed param): ${bootstrapdsx_ifacectlplane}"
+logger "bootstrapdsx_configure.bash: Registration Port (passed param): ${bootstrapdsx_regport}"
+logger "bootstrapdsx_configure.bash: REST API Port (passed param): ${bootstrapdsx_restport}"
 #set +x
--- deflect_configure.sh
+++ deflect_configure.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-logger "deflect_configure: CONFIGURATION of the Deflect"
+logger "deflect_configure: Bootstrap DSX CONFIGURATION - Some parms passed in from Deflect!"
 
-logger "deflect_configure: Hostname: ${deflect_hostname}"
-logger "deflect_configure: Traffic IP Address: ${deflect_dflnet}" 
-logger "deflect_configure: Traffic Interface: ${deflect_ifacetraffic}" 
-#logger "deflect_configure: DSX IP Address: bootstrapdsx_dsxnet"
+logger "deflect_configure: Hostname: ${hostname}"
+logger "deflect_configure: IP Address: ${dflnet}" 
+logger "deflect_configure: Ctl Plane Interface: ${ifacectlplane}" 
+logger "deflect_configure: DSX IP Address: ${bootstrapdsx_dsxnet}"
+logger "deflect_configure: DSX Reg Port (passed param) ${bootstrapdsx_regport}"
+logger "deflect_configure: DSX REST API Port (passed param): ${bootstrapdsx_restport}"
