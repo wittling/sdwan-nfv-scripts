4f40672a55c414045f6ee69d679127cca90ace4d
wittling <mwittling@dispersivegroup.com>
2017-10-02 18:04:08 -0400

    svc group type introduced

--- bootstrapdsx_instantiate.sh
+++ bootstrapdsx_instantiate.sh
@@ -239,6 +239,7 @@ logger "bootstrapdsx_instantiate: Registration Port: ${portreg}"
 logger "bootstrapdsx_instantiate: REST RA Port: ${portra}" 
 logger "bootstrapdsx_instantiate: REST RW Port: ${portrw}" 
 logger "bootstrapdsx_instantiate: Service Group: ${svcgroup}" 
+logger "bootstrapdsx_instantiate: Service Group: ${svcgrptyp}" 
 
 logger "bootstrapdsx_instantiate: Changing IP Address in CFGTMPL: ${dsxnet}" 
 jsonParmSwap CFGTMPL ${dsxnet}
@@ -334,7 +335,7 @@ if [ $? -eq 0 ]; then
          fi
 
          logger "bootstrapdsx_instantiate: INFO: Attempting to provision new OPENBATON service group." 
-         (python3 servicegroup.py ${svcgroup} ${svcgroup} l3mlx 1>servicegroup.py.log 2>&1)
+         (python3 servicegroup.py ${svcgroup} ${svcgroup} ${svcgrptyp} 1>servicegroup.py.log 2>&1)
          if [ $? -eq 0 ]; then
             logger "bootstrap_instantiate:INFO: Service Group OPENBATON provisioned!"
          elif [ $? -eq 4 ]; then
