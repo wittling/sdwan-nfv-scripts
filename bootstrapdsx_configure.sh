844b32797b9749fae9fc563095268f2ef03290ed
wittling <mwittling@dispersivegroup.com>
2017-10-04 21:57:56 -0400

    moved logic for vtc to the deflect configure script

--- bootstrapdsx_configure.sh
+++ bootstrapdsx_configure.sh
@@ -16,52 +16,8 @@ logger "bootstrapdsx_configure.bash: My REST API Port is: ${bootstrapdsx_portres
 
 # export the variables
 export hostname
-
-logger "bootstrapdsx_configure:INFO: Attempting to provision VTC via REST interface"
-python3 -V
-if [ $? -eq 0 ]; then
-   RESTCLTDIR="/usr/local/dart-rest-client/local-client-projects"
-   if [ -d ${RESTCLTDIR} ]; then
-      pushd ${RESTCLTDIR}
-      CLASSFILE=rxtxnode
-      if [ -f ${CLASSFILE}.py ]; then
-         if [ ! -x ${CLASSFILE}.py ]; then
-            chmod +x ${CLASSFILE}.py
-         fi
-         DVNRESTENV=".dvnrestenv"
-         if [ -f ${DVNRESTENV} ]; then
-            logger "bootstrapdsx_configure:INFO: Sourcing rest environment..."
-            source "${DVNRESTENV}"
-         else
-            logger "bootstrapdsx_configure:INFO: Sourcing rest environment..."
-            popd
-            exit 1
-         fi
-
-         logger "bootstrapdsx_configure: INFO: Attempting to provision new vtc ${hostname}."
-         (python3 ${CLASSFILE}.py ${hostname} ${hostname} "no" 1>${CLASSFILE}.py.log 2>&1)
-         if [ $? -eq 0 ]; then
-            logger "bootstrap_configure:INFO: VTC ${hostname} provisioned!"
-         elif [ $? -eq 4 ]; then
-            logger "bootstrap_configure:INFO: VTC ${hostname} already provisioned (assumed correct)."
-         else
-            logger "bootstrap_configure:ERROR: Error in attempt to provision VTC ${hostname}. Shell Code: $?"
-            popd
-            exit 1
-         fi
-      else
-         logger "bootstrap_configure:ERROR: FileNotExists: ${CLASSFILE}"
-         popd
-         exit 1
-      fi
-   else
-      logger "bootstrap_configure:ERROR: DirNotExists: ${RSTCLTDIR}"
-      exit 1
-   fi
-else
-   logger "bootstrap_configure:ERROR: FileNotExists: Python3 Not Installed"
-   exit 1
-fi
+export bootstrapdsx_portreg
+export bootstrapdsx_portrest
 
 logger "bootstrap_configure:INFO: Successful implementation of bootstrapdsx_configure script. Exiting 0."
 exit 0
