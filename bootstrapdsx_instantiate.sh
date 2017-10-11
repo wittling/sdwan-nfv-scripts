5299bd860d870ff9dcb86924efaf6368c792a801
wittling <mwittling@dispersivegroup.com>
2017-10-11 11:47:42 -0400

    fix script suffix on log statements

--- bootstrapdsx_instantiate.sh
+++ bootstrapdsx_instantiate.sh
@@ -118,29 +118,29 @@ SVCNAME="${UNITNAME}@bootstrap.service"
 DBINITIALIZE=8
 
 # Go ahead and shut down the DPS Services so that we can start everything in proper sequence.
-logger "Script: bootstrapdsx_instantiate.bash: Stopping DPS Services so we can start Percona bootstrap"
+logger "Script: bootstrapdsx_instantiate.sh: Stopping DPS Services so we can start Percona bootstrap"
 systemctl stop dps.service
 OUTPUT=`systemctl is-active dps.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:WARNING: dps.service did not stop. non-fatal. We will continue."
+   logger "Script: bootstrapdsx_instantiate.sh:WARNING: dps.service did not stop. non-fatal. We will continue."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dps.service stopped."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dps.service stopped."
 fi
 
 systemctl stop dart-rest.service
 OUTPUT=`systemctl is-active dart-rest.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:WARNING: dart-rest.service did not stop. non-fatal. We will continue."
+   logger "Script: bootstrapdsx_instantiate.sh:WARNING: dart-rest.service did not stop. non-fatal. We will continue."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dart-rest.service stopped."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dart-rest.service stopped."
 fi
 
 systemctl stop dart3.service
 OUTPUT=`systemctl is-active dart3.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:WARNING: dart3.service did not stop. non-fatal. We will continue."
+   logger "Script: bootstrapdsx_instantiate.sh:WARNING: dart3.service did not stop. non-fatal. We will continue."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dart3.service stopped."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dart3.service stopped."
 fi
 
 # Since we are not completely sure whether the DB is running in primary clustered mode or not, we will need to 
@@ -150,20 +150,20 @@ systemctl stop ${UNITNAME}
 sleep 3
 OUTPUT=`systemctl is-active ${UNITNAME}`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: ${UNITNAME}.service did not stop. non-fatal. We will continue."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: ${UNITNAME}.service did not stop. non-fatal. We will continue."
    exit 1
 else
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: ${UNITNAME}.service stopped."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: ${UNITNAME}.service stopped."
 fi
 
 systemctl stop ${SVCNAME}
 sleep 3
 OUTPUT=`systemctl is-active ${SVCNAME}`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: ${SVCNAME}.service did not stop. non-fatal. We will continue."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: ${SVCNAME}.service did not stop. non-fatal. We will continue."
    exit 1
 else
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: ${SVCNAME}.service stopped."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: ${SVCNAME}.service stopped."
 fi
 
 # Check and make sure mysql is not running. If it is, shut it down.
@@ -172,7 +172,7 @@ OUTPUT=`pgrep -lf ${PROCESS}`
 # If we do not stop the service and just kill the process it may just restart on us.
 # So make sure we put a stop on it from a service perspective first.
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash: Detected mysql running. Attempting to stop..."
+   logger "Script: bootstrapdsx_instantiate.sh: Detected mysql running. Attempting to stop..."
    systemctl stop ${UNITNAME}
    systemctl stop ${SVCNAME}
    sleep 3
@@ -181,35 +181,35 @@ fi
 # Check it again. If we see it kill it.
 OUTPUT=`pgrep -lf ${PROCESS}`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash: mysql STILL running. Attempting to kill it..."
+   logger "Script: bootstrapdsx_instantiate.sh: mysql STILL running. Attempting to kill it..."
    pkill ${PROCESS}
    sleep 2
 fi
 
 # Start the bootstrap
-logger "Script: bootstrapdsx_instantiate.bash: Attempting to start Percona as bootstrap..."
+logger "Script: bootstrapdsx_instantiate.sh: Attempting to start Percona as bootstrap..."
 systemctl start ${SVCNAME}
 
-logger "Script: bootstrapdsx_instantiate.bash:INFO: Percona takes a while to initialize..."
-logger "Script: bootstrapdsx_instantiate.bash:INFO: Waiting ${DBINITIALIZE}..."
+logger "Script: bootstrapdsx_instantiate.sh:INFO: Percona takes a while to initialize..."
+logger "Script: bootstrapdsx_instantiate.sh:INFO: Waiting ${DBINITIALIZE}..."
 sleep ${DBINITIALIZE} 
 
 for i in 1 2 3; do
 OUTPUT=`systemctl is-active ${SVCNAME}`
-logger "Script: bootstrapdsx_instantiate.bash:DEBUG: Return code from is-active on ${SVCNAME}: ${OUTPUT}"
+logger "Script: bootstrapdsx_instantiate.sh:DEBUG: Return code from is-active on ${SVCNAME}: ${OUTPUT}"
 RC=$?
 if [ ${RC} -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: Service ${SVCNAME} reported as active..."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: Service ${SVCNAME} reported as active..."
    break
 elif [ ${RC} -eq 1 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: Service ${SVCNAME} trying to activate..."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: Service ${SVCNAME} trying to activate..."
    sleep 2
 else
    echo "ERROR: Service ${SVCNAME} NOT reported as active." 
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: Service ${SVCNAME} NOT reported as active..."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: Service ${SVCNAME} NOT reported as active..."
    if [ $i -eq 3 ]; then
-      logger "Script: bootstrapdsx_instantiate.bash:ERROR: Manual intervention required to start ${SVCNAME} properly!"
-      logger "Script: bootstrapdsx_instantiate.bash:ERROR: Giving up on required service ${SVCNAME}. Exiting."
+      logger "Script: bootstrapdsx_instantiate.sh:ERROR: Manual intervention required to start ${SVCNAME} properly!"
+      logger "Script: bootstrapdsx_instantiate.sh:ERROR: Giving up on required service ${SVCNAME}. Exiting."
       exit 1
    fi
 fi
@@ -225,9 +225,9 @@ done
 #SHOW STATUS LIKE 'wsrep_%';
 #EOS
 #if [ $? -eq 0 ]; then
-#   logger "Script: bootstrapdsx_instantiate.bash: INFO: Percona is running as a Primary Node!"
+#   logger "Script: bootstrapdsx_instantiate.sh: INFO: Percona is running as a Primary Node!"
 #else
-#   logger "Script: bootstrapdsx_instantiate.bash: ERROR: Percona is NOT running as a Primary Node!"
+#   logger "Script: bootstrapdsx_instantiate.sh: ERROR: Percona is NOT running as a Primary Node!"
 #   exit 1
 #fi
 
@@ -285,14 +285,14 @@ fi
 
 # Now restart the DPS Services
 
-logger "Script: bootstrapdsx_instantiate.bash:INFO: Restarting dps.service after Percona Bootstrap startup."
+logger "Script: bootstrapdsx_instantiate.sh:INFO: Restarting dps.service after Percona Bootstrap startup."
 systemctl start dps.service
 sleep 3
 OUTPUT=`systemctl is-active dps.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dps.service restarted."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dps.service restarted."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: dps.service did NOT restart. Manual intervention required."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: dps.service did NOT restart. Manual intervention required."
    exit 1 
 fi
 
@@ -300,18 +300,18 @@ systemctl start dart3.service
 sleep 3
 OUTPUT=`systemctl is-active dart3.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dart3.service (node) restarted."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dart3.service (node) restarted."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: dart3.service (node) did NOT restart. Manual intervention required."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: dart3.service (node) did NOT restart. Manual intervention required."
    exit 1 
 fi
 
 systemctl start dart-rest.service
 OUTPUT=`systemctl is-active dart-rest.service`
 if [ $? -eq 0 ]; then
-   logger "Script: bootstrapdsx_instantiate.bash:INFO: dart-rest.service (node) restarted."
+   logger "Script: bootstrapdsx_instantiate.sh:INFO: dart-rest.service (node) restarted."
 else
-   logger "Script: bootstrapdsx_instantiate.bash:ERROR: dart-rest.service (node) did NOT restart. Manual intervention required."
+   logger "Script: bootstrapdsx_instantiate.sh:ERROR: dart-rest.service (node) did NOT restart. Manual intervention required."
    exit 1 
 fi
 
