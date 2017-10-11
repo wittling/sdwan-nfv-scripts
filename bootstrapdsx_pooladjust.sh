c9ef53877b04bff1b4e8644c5d7be8865211c973
wittling <mwittling@dispersivegroup.com>
2017-10-11 11:22:30 -0400

    added new script for pool size adjust

---
+++ bootstrapdsx_pooladjust.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# It appears that this script gets cranked for every deflect that comes up.
+# If this is in fact how the orchestrator is doing this, we can take advantage of this
+# by making RESTful API calls to provision each one at their time of instantiation.
+
+#env
+#set -x
+
+logger "bootstrapdsx_pooladjust:INFO: We will adjust our deflect pool size here."
+
+#set +x
+exit 0
